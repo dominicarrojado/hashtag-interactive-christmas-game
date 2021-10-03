1b137c1f050b4f40631140a98feda0e1c2fdf685
Dominic Arrojado <dominicarrojado@gmail.com>
2021-10-03 11:29:52 +0800

    clean up project and initial setup

---
+++ pages/_app.page.tsx
@@ -0,0 +1,8 @@
+import 'normalize.css';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+export default App;
---
+++ pages/index.page.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  return <div className={styles.container}>Hello World</div>;
+}
+
+export default Home;
