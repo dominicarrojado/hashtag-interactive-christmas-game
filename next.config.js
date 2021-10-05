5947c8aadb7f273e5ec1106c557aa5c79f95322b
Dominic Arrojado <dominicarrojado@gmail.com>
2021-10-05 13:33:29 +0800

    add assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   pageExtensions: ['page.tsx'],
   basePath: '/hashtag-interactive-christmas-game',
+  assetPrefix: isProd ? '/hashtag-interactive-christmas-game' : '',
 };
